8198ae411e88530d40c811ec5bc5b4bb2ee1e638
Robert Maxwell <robertjamesmaxwell@gmail.com>
2021-12-17 21:24:49 -0600

    Sessions csv

---
+++ script.js
@@ -0,0 +1,43 @@
+// Import Dependencies
+const fs = require('fs');
+const converter = require('json-2-csv');
+
+// Import Data
+const sessionsData = JSON.parse(fs.readFileSync('./inputData/sessions.json'));
+// console.log("SESSIONS: ", Object.values(sessionsData.sessionProducts)[1]);
+const speakersData = JSON.parse(fs.readFileSync('./inputData/speakers.json'));
+// console.log("SESSIONS: ", Object.values(speakersData.speakers)[1]);
+
+//Utilities
+const stripHTML = str => str.replace(/<[^>]+>/g, '');
+const stripLineBreaks = someText => someText.replace(/(\r\n|\n|\r)/gm, "");
+const parseToCsv = async (jsonData, fileName) => {
+  try {
+    const csv = await converter.json2csvAsync(jsonData);
+    // console.log("CSV", csv);
+    fs.writeFileSync(`./output/${fileName}`, csv);
+    console.log(`Wrote to CSV: ${fileName}`)
+  } catch (err) {
+      console.log("ERROR: ", err);
+  }
+} 
+
+// Sessions
+const mappedSessions = Object.values(sessionsData.sessionProducts)
+  .map(({
+      name, type, plainTextDescription, startTime, endTime, speakerIds
+    }) => ({
+    name,
+    type,
+    startTime: new Date(startTime).toLocaleString(),
+    endTime: new Date(endTime).toLocaleString(),
+    description: stripLineBreaks(plainTextDescription),
+    speakerNames: Object.keys(speakerIds).map(speakerId => {
+      return speakersData.speakers[speakerId].firstName + " " + speakersData.speakers[speakerId].lastName
+    }).join(" ; ")
+  }))
+
+console.log("mappedSessions: ", mappedSessions[3]);
+parseToCsv(mappedSessions, "sessions.csv")
+
+
